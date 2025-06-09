613baf3b7ea4c6f005602afb42d0934404611adb
izubkov <izubkov.forall@gmail.com>
2025-06-09 14:06:55 +0200

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js on DigitalOcean App Platform!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
