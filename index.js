1ed02ce4b3d275bc4e194331d55a652ecbe98405
izubkov <izubkov.forall@gmail.com>
2025-06-09 19:36:40 +0200

    added Goose CLI

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const express = require('express');
+const { exec } = require('child_process');
+
+// Create router
 const router = express.Router();
 
+// Import route handlers
+const { simulate } = require('.controllers/simulateController');
+const { healthCheck } = require('.controllers/healthController');
 
 // Routes
 router.get('/simulate', simulate);
 router.get('/health', healthCheck);
+
+// Create Express app
+const app = express();
+
+// Use your custom routes
+app.use('/', router);
+
+// Goose version endpoint
+app.get('/goose-version', (req, res) => {
+  exec('goose --version', (error, stdout, stderr) => {
+    if (error) return res.status(500).send(error.message);
+    if (stderr) return res.status(500).send(stderr);
+    res.send(stdout);
+  });
+});
+
+app.listen(8080, () => console.log('Server running on 8080'));
 
 module.exports = router;
